405ac74168cd3478aa71cdf7ed8f42015945c5dc
Khiem Doan <doankhiem.crazy@gmail.com>
2023-10-04 17:20:18 +0700

    Exception must be handle in task group

--- docs_src/tutorial/soonify_return/tutorial002.py
+++ docs_src/tutorial/soonify_return/tutorial002.py
@@ -9,11 +9,19 @@ async def do_work(name: str):
 
 
 async def get_data():
+    ex = None
+
     async with asyncer.create_task_group() as task_group:
         soon_value1 = task_group.soonify(do_work)(name="Yury")
         soon_value2 = task_group.soonify(do_work)(name="Nathaniel")
         soon_value3 = task_group.soonify(do_work)(name="Alex")
-        print(soon_value1.value)
+        try:
+            print(soon_value1.value)
+        except asyncer.PendingValueException as _ex:
+            ex = _ex
+
+    if ex is not None:
+        raise ex
 
     data = [soon_value1.value, soon_value2.value, soon_value3.value]
     return data
