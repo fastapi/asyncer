df763ae417a185fe01b6ba8d6fcfe9d494d096ac
pejmans21 <pezhmansamadi21@gmail.com>
2023-10-24 08:23:55 +0330

    [scripts/lint.sh] lint.sh use ruff instead of flake8 & isort.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,5 @@ set -e
 set -x
 
 mypy asyncer
-flake8 asyncer tests docs_src
+ruff check asyncer tests docs_src
 black asyncer tests docs_src --check
-isort asyncer tests docs_src scripts --check-only
