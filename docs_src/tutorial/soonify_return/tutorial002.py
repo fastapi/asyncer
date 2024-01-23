2b074eace279e870dd26840df7c68971db17f175
Sebastián Ramírez <tiangolo@gmail.com>
2024-01-23 19:34:31 +0100

    📝 Revert changing tutorial

--- docs_src/tutorial/soonify_return/tutorial002.py
+++ docs_src/tutorial/soonify_return/tutorial002.py
@@ -9,19 +9,11 @@ async def do_work(name: str):
 
 
 async def get_data():
-    ex = None
-
     async with asyncer.create_task_group() as task_group:
         soon_value1 = task_group.soonify(do_work)(name="Yury")
         soon_value2 = task_group.soonify(do_work)(name="Nathaniel")
         soon_value3 = task_group.soonify(do_work)(name="Alex")
-        try:
-            print(soon_value1.value)
-        except asyncer.PendingValueException as _ex:
-            ex = _ex
-
-    if ex is not None:
-        raise ex
+        print(soon_value1.value)
 
     data = [soon_value1.value, soon_value2.value, soon_value3.value]
     return data
