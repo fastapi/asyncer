bf9672cce73353ace0cb1f3155b1206785a21905
Daniel Porter <dpreid@gmail.com>
2024-09-19 17:59:36 +0100

    Use inspect to determine anyio.to_thread.run_sync kwargs

--- asyncer/_compat.py
+++ asyncer/_compat.py
@@ -1,26 +1,17 @@
-import sys
-from typing import Callable, Tuple, TypeVar, Union
+import inspect
+from typing import Callable, TypeVar, Union
 
 import anyio
 import anyio.to_thread
 from anyio import CapacityLimiter
 from typing_extensions import TypeVarTuple, Unpack
 
-if sys.version_info < (3, 10):
-    from importlib_metadata import version as get_version
-else:
-    from importlib.metadata import version as get_version
-
-ANYIO_VERSION: Tuple[int, ...] = tuple(
-    int(num) for num in get_version("anyio").split(".")[:3]
-)
-
 T_Retval = TypeVar("T_Retval")
 PosArgsT = TypeVarTuple("PosArgsT")
 
 
 # AnyIO 4.1.0 renamed cancellable to abandon_on_cancel
-if ANYIO_VERSION >= (4, 1):
+if "abandon_on_cancel" in inspect.getfullargspec(anyio.to_thread.run_sync).kwonlyargs:
 
     async def run_sync(
         func: Callable[[Unpack[PosArgsT]], T_Retval],
