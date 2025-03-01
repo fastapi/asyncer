022707704b01b95f6ffe1323357f2ff376d72f53
Daniel Porter <dpreid@gmail.com>
2025-03-01 02:04:07 +0000

    Move anyio kwarg check to constant
    This ensures the check is only performed once on import, whilst
    retaining a static function definition.

--- asyncer/_compat.py
+++ asyncer/_compat.py
@@ -9,6 +9,11 @@ from typing_extensions import TypeVarTuple, Unpack
 T_Retval = TypeVar("T_Retval")
 PosArgsT = TypeVarTuple("PosArgsT")
 
+# Use abandon_on_cancel if available
+RUN_SYNC_AOC = (
+    "abandon_on_cancel" in inspect.getfullargspec(anyio.to_thread.run_sync).kwonlyargs
+)
+
 
 async def run_sync(
     func: Callable[[Unpack[PosArgsT]], T_Retval],
@@ -17,10 +22,7 @@ async def run_sync(
     limiter: Union[CapacityLimiter, None] = None,
 ) -> T_Retval:
     # AnyIO 4.1.0 renamed cancellable to abandon_on_cancel
-    if (
-        "abandon_on_cancel"
-        in inspect.getfullargspec(anyio.to_thread.run_sync).kwonlyargs
-    ):
+    if RUN_SYNC_AOC:
         return await anyio.to_thread.run_sync(
             func, *args, abandon_on_cancel=abandon_on_cancel, limiter=limiter
         )
